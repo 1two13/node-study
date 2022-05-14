d9986cbeb5ca315bf68195ecc733eef686b8d49b
Tory <88531407+1two13@users.noreply.github.com>
2022-05-14 15:09:21 +0900

    feature: 답안 수정

--- HW35.js
+++ HW35.js
@@ -8,25 +8,26 @@ function main() {
   let result = "";
   // n은 1 ~ 100
   for (let n = 1; n <= 100; n++) {
-    // 만약 n이 3의 배수인 경우에는 * 로 표시하기
-    if (n % 3 === 0) result += ` *`;
-    // 만약 n이 5의 배수인 경우에는 # 으로 표시하기
-    else if (n % 5 === 0) result += ` #`;
+    // 아래 3가지 경우의 수를 제외한 수는 모두 그냥 숫자로 표시하기
+    let char = n.toString();
     // n이 3의 배수이면서 5의 배수인 경우에는 그냥 숫자로 표시하기
-    // 위 3가지 경우의 수를 제외한 수는 모두 그냥 숫자로 표시하기
-    else result += " " + n;
-  }
+    if (n % 15 === 0) char = char;
+    // 만약 n이 3의 배수인 경우에는 * 로 표시하기
+    else if (n % 3 === 0) char = "*";
+    // 만약 n이 5의 배수인 경우에는 # 으로 표시하기
+    else if (n % 5 === 0) char = "#";
 
-  // 한 줄에 10개만 출력해야하는데.. => slice를 사용해서 10개씩 자르기
-  for (let i = 1; i < result.split(" ").length; i += 10) {
-    console.log(
-      " " +
-        result
-          .split(" ")
-          .slice(i, i + 10)
-          .join(" ")
-    );
+    // n.toString().length === 1 이면 n 앞 뒤로 띄어쓰기를 주기
+    if (char.length === 1) result += " " + char + " ";
+    // n.toString().length === 2 면 n 뒤에만 띄어쓰기를 주기
+    else if (char.length === 2) result += char + " ";
+
+    // 한 줄에 10개의 값만 보여주기
+    if (n % 10 === 0) {
+      result += "\n";
+    }
   }
+  console.log(result);
 }
 
 main();
